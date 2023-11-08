db595f43315cf3bc3fe94f7d076522b72fcabf10
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-11-07 22:58:31 -0300

    Arquivo Layout com a estrutura

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
+import Link from 'next/link'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import Cabecalho from '../components/Cabecalho/Cabecalho.jsx'
+import Rodape from '../components/Rodape/Rodape.jsx'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +14,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* <div>
+            <Link href="/pages/sobre">SOBRE</Link>
+            <Link href="/pages/ajuda">AJUDA</Link>
+        </div> */}
+        <Cabecalho />
+
+
+        {children}
+        
+        <Rodape />
+        {/* <p>RODAPÉ</p> */}
+      </body>
     </html>
   )
 }
